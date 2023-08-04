b00a29ce412de9be07aa4735468c490c9c258546
LuckyAnggara <anggara.lucky1992@gmail.com>
2023-08-04 15:56:35 +0700

    kantor

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'employee_id' => 1,
+            'username' => 'lucky',
+            'password' => '$2y$10$SKX.kBjyiXwCnnEo2jMsD.aCIuKhS52PeLqsQc21N7Ix3MRZ8rB6O',
+            'branch_id' => 1,
+            'role_id' => 4,
+        ]);
+    }
+}
