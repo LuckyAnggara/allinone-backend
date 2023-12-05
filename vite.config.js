752ba0a8e176d277e5645439ea2532d9197404e6
kerjaanlucky <kerjaanlucky@gmail.com>
2023-12-05 20:14:46 +0700

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,7 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    base: "/",
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
-        },
-    },
-    build: {
-        chunkSizeWarningLimit: 1600,
-    },
     plugins: [
-        vue(),
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
